676a46cc120e3d366752568cc359a6d35d6b998d
AgentSmith04 <kevnivesela@gmail.com>
2025-10-31 08:48:10 -0500

    Agrega entidades y migraciones

--- src/modules/categoria/entities/categoria.entity.ts
+++ src/modules/categoria/entities/categoria.entity.ts
@@ -1,13 +1,18 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Producto } from "../../producto/entities/producto.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity()
 export class Categoria {
     @PrimaryGeneratedColumn()
-    id: number;
+  id: number;
+  
     @Column()
     nombre:string;
 
     @Column()
-    detalle:string;
+  detalle: string;
+  
+  @OneToMany(() => Producto, (prod) => prod.categoria)
+  producto:Producto[]
 }
